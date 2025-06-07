5a3bd350f6b8985f7d6409cc1a12a91c3108b30c
sager <xueqian1991@gmail.com>
2025-06-08 06:49:06 +0800

    fix bug

--- library/liveness/liveness.go
+++ library/liveness/liveness.go
@@ -126,7 +126,7 @@ func (o *Instance) Run(ctx context.Context, params types.LibraryParams, feedback
 					}
 
 					if params.IsHackValidator(toInt(nextDuty[0].ValidatorIndex)) && params.IsHackValidator(toInt(curDuty[0].ValidatorIndex)) &&
-						o.attackerInTailN(params.FilterHackerDuties(curDuty)[0], 5) {
+						o.attackerInTailN(params.FilterHackerDuties(curDuty), 5) {
 						triggerring = true
 						triggeredEpoch = int(epoch)
 						olog.WithFields(log.Fields{
@@ -450,8 +450,9 @@ func (o *Instance) attackerCount(vals []primitives.ValidatorIndex) int {
 	return count
 }
 
-func (o *Instance) attackerInTailN(latestAttackDuty types.ProposerDuty, tailN int) bool {
-	slot := toInt(latestAttackDuty.Slot)
+func (o *Instance) attackerInTailN(attackDuties []types.ProposerDuty, tailN int) bool {
+	latest := attackDuties[len(attackDuties)-1]
+	slot := toInt(latest.Slot)
 	epoch := common.SlotToEpoch(int64(slot))
 	epochEnd := common.EpochEnd(epoch)
 	return (int(epochEnd) - tailN) <= slot
